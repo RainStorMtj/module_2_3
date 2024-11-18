bcf6fc112ec9738acf1915a50211e928400787bc
RainStorMtj <145570462+RainStorMtj@users.noreply.github.com>
2024-11-19 00:27:42 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,12 +1,11 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-a = 0
-while a < len(my_list):
-    if my_list[a] == 0:
-        a += 1
+init_value = 0
+while init_value < len(my_list):
+    if my_list[init_value] == 0:
+        init_value += 1
         continue
-        print(my_list[a])
-    elif my_list[a] > 0:
-        print(my_list[a])
-        a += 1
-    elif my_list[a] <= -1:
+    elif my_list[init_value] > 0:
+        print(my_list[init_value])
+        init_value += 1
+    elif my_list[init_value] <= -1:
         break
